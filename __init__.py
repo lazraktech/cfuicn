d5304226091da204325e55d1ae0f1f1d5c3876cb
Sam <sam@Mac.lan>
2024-12-03 07:04:44 -0600

    init

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .myfirstnode import MyNode
+NODE_CLASS_MAPPINGS = {"MyNode": MyNode}
+NODE_DISPLAY_NAME_MAPPINGS = {"FirstNode": "My First Node"}
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
